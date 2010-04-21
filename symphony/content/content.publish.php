4e94663d6af0ae8bf9a8bfceb7402131403cfdde
Brendan Abbott <brendan@bloodbone.ws>
2010-04-21 15:29:06 +1000

    Quick cleanup of Prepopulate

--- symphony/content/content.publish.php
+++ symphony/content/content.publish.php
@@ -746,15 +746,10 @@
 			$this->entry->findDefaultFieldData();
 			$this->Form->appendChild(Widget::Input('MAX_FILE_SIZE', Symphony::Configuration()->get('max_upload_size', 'admin'), 'hidden'));
 
-			//var_dump($_REQUEST, $_POST);die();
 			// Check if there is a field to prepopulate
-			if (isset($_REQUEST['prepopulate'])) {
+			if (isset($_REQUEST['prepopulate']) && strlen(trim($_REQUEST['prepopulate'])) > 0) {
 				$field_handle = key($_REQUEST['prepopulate']);
 				$value = stripslashes(rawurldecode($_REQUEST['prepopulate'][$field_handle]));
-
-				$this->Form->prependChild(
-					Widget::Input("prepopulate[{$field_handle}]",rawurlencode($value),'hidden')
-				);
 
 				$prepopulate_filter = "?prepopulate[{$field_handle}]=" . rawurlencode($value);
 				$this->Form->setAttribute('action', Administration::instance()->getCurrentPageURL() . $prepopulate_filter);
