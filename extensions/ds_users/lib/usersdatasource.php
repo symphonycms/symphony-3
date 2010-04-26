5e4043bfcbc998c57b8f8334d664c52ab41088aa
Alistair Kearney <alistair@21degrees.com.au>
2010-04-26 10:00:20 +1000

    Fixed filtering on the Users DS type

--- extensions/ds_users/lib/usersdatasource.php
+++ extensions/ds_users/lib/usersdatasource.php
@@ -30,7 +30,7 @@
 			$root = $result->createElement($this->parameters()->{'root-element'});
 
 			try {
-				$user_ids = array();
+				$user_ids = NULL;
 
 				##	User Filtering
 				//	TODO: Check that this is working once Duplicators are ready
@@ -38,65 +38,88 @@
 					foreach ($this->parameters()->filter as $field => $value){
 						if(!is_array($value) && trim($value) == '') continue;
 
-						$ret = $this->processUserFilter($field, $value);
-
-						if(empty($ret)){
-							$user_ids = array();
-							break;
-						}
-
-						if(empty($user_ids)) {
-							$user_ids = $ret;
+						$ret = $this->processUserFilter($field, $this->replaceParametersInString($value, $ParameterOutput));
+						
+						
+						if(!is_null($ret)){
+							$user_ids = (!is_null($user_ids) ? array_intersect($user_ids, $ret) : $ret);
 							continue;
 						}
-
-						$user_ids = array_intersect($user_ids, $ret);
-
+						
+						// Since this is an "AND" operation, if $user_ids is ever 
+						// empty, it means one of the filters has returned no results
+						break;
+						
 					}
-
-					$users = Symphony::Database()->query(
-						"SELECT * FROM `tbl_users` WHERE `id` IN (%s) ORDER BY `id` ASC", 
-						array(
-							implode(',', array_values($user_ids))
-						),
-						'UserResult'
-					);
-
+					
+					if(!empty($user_ids)){
+						$users = Symphony::Database()->query(
+							"SELECT * FROM `tbl_users` WHERE `id` IN (%s) ORDER BY `id` ASC", 
+							array(
+								implode(',', array_values($user_ids))
+							),
+							'UserResult'
+						);
+					}
+					
 				}
 
 				else {
 					$users = new UserIterator;
 				}
 
-				if(is_array($users) && !empty($users)) {
+				if(is_object($users) && $users->length() > 0){
+
+					$included_fields = $this->parameters()->{'included-elements'};
+
 					foreach($users as $user) {
 						$xUser = $result->createElement('user', null);
 						$xUser->setAttribute('id', $user->id);
 
-						$fields = array(
-							'name' => $result->createElement('name', $user->getFullName()),
-							'username' => $result->createElement('username', $user->username),
-							'email' => $result->createElement('email', $user->email)
-						);
+						foreach($included_fields as $element){
 
-						if($user->isTokenActive()) {
-							$fields['authentication-token'] = $result->createElement('authentication-token', $user->createAuthToken());
-						}
+							switch($element){
+								
+								case 'name':
+									$xUser->appendChild($result->createElement('name', $user->getFullName()));
+									break;
+									
+								case 'email-address':
+									$xUser->appendChild($result->createElement('email-address', $user->email));
+									break;
+									
+								case 'username':
+									$xUser->appendChild($result->createElement('username', $user->username));
+									break;
+								
+								case 'language':
+									if(!is_null($user->language)) $xUser->appendChild($result->createElement('language', $user->language));
+									break;
+									
+								case 'authentication-token':
+									if($user->isTokenActive()) $xUser->appendChild($result->createElement('authentication-token', $user->createAuthToken()));
+									break;
+									
+								case 'default-section':
 
-						try {
-							$section = Section::loadFromHandle($user->default_section);
+									try {
+										$section = Section::loadFromHandle($user->default_section);
 
-							$default_section = $result->createElement('default-section', $section->name);
-							$default_section->setAttribute('handle', $section->handle);
+										$default_section = $result->createElement('default-section', $section->name);
+										$default_section->setAttribute('handle', $section->handle);
 
-							$fields['default-section'] =  $default_section;
-						}
-						catch (SectionException $error) {
-							// Do nothing, section doesn't exist, but no need to error out about it..
-						}
-
-						foreach($fields as $field) {
-							$xUser->appendChild($field);
+										$xUser->appendChild($default_section);
+									}
+									catch (SectionException $error) {
+										// Do nothing, section doesn't exist, but no need to error out about it..
+									}
+								
+									break;
+									
+								default:
+									break;
+								
+							}
 						}
 
 						$root->appendChild($xUser);
@@ -113,7 +136,7 @@
 			}
 
 			catch (Exception $error) {
-				$root->appendChild($doc->createElement(
+				$root->appendChild($result->createElement(
 					'error', General::sanitize($error->getMessage())
 				));
 			}
@@ -135,24 +158,13 @@
 				$bits = $filter;
 			}
 
-			$query = sprintf(
-				"
-					SELECT
-						`id`
-					FROM
-						`tbl_users`
-					WHERE
-						`%s` IN ('%%s')
-				",
-				$field
-			);
+			$query = sprintf("SELECT `id` FROM `tbl_users` WHERE `%s` IN ('%%s')", $field);
 
 			$result = Symphony::Database()->query($query, array(
 				implode("','", $bits)
 			));
-			$users = $result->resultColumn('id');
 
-			return (is_array($users) && !empty($users) ? $users : NULL);
+			return ($result->length() > 0 ? $result->resultColumn('id') : NULL);
 		}
 
 		public function prepareSourceColumnValue() {
