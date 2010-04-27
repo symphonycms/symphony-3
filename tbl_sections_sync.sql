0c4009553e2e3196caccea9fa08bfe37da3844b7
Rowan Lewis <rowan@randb.com.au>
2010-04-27 14:05:46 +1000

    Added new section sync table definition.

--- tbl_sections_sync.sql
+++ tbl_sections_sync.sql
@@ -1,8 +1,5 @@
 CREATE TABLE `tbl_sections_sync` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `guid` varchar(32) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `type` varchar(255) DEFAULT NULL,
-  `section` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=35 DEFAULT CHARSET=utf8;
+  `section` varchar(32) NOT NULL DEFAULT '',
+  `xml` text,
+  PRIMARY KEY (`section`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
