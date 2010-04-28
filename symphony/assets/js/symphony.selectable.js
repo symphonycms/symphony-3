5f958c8906d285af80379d6a7fe1f158ae9c7e80
Rowan Lewis <rowan@randb.com.au>
2010-04-28 17:22:51 +1000

    Cleaned up old table selecting.

--- symphony/assets/js/symphony.selectable.js
+++ symphony/assets/js/symphony.selectable.js
@@ -83,63 +83,6 @@
 				return false;
 			});
 			
-			/*
-			var items = object.find(settings.items);
-			var last = null;
-			
-			object.bind('selectable-refresh', function() {
-				items.each(function() {
-					var item = jQuery(this);
-					var input = item.find('input:first');
-					
-					input.get(0).checked = item.is('.selected');
-				});
-			});
-			
-			// Prevent shift clicking from selecting text:
-			items.live('mousedown', function(e) {
-				return !e.shiftKey;
-			});
-			
-			items.live('click', function(e) {
-				var self = jQuery(this);
-				
-				// Ignore link clicks:
-				if (e.target instanceof HTMLAnchorElement) {
-					return true;
-				}
-				
-				if (e.shiftKey) {
-					var state = self.is('.selected');
-					var from = items.index(last);
-					var to = items.index(self);
-					var slice = items.slice(
-						Math.min(from, to),
-						Math.max(from, to)
-					).add(self);
-					
-					if (last) {
-						state = last.is('.selected');
-						last = null;
-					}
-					
-					if (state) {
-						slice.addClass('selected');
-					}
-					
-					else {
-						slice.removeClass('selected');
-					}
-				}
-				
-				else {
-					last = self.toggleClass('selected');
-				}
-				
-				object.trigger('selectable-refresh');
-			});
-			*/
-
 			return object;
 		});
 
