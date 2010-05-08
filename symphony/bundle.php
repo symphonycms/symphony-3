963515726d2ed98f6c1d691c5f6488d0d89edace
Rowan Lewis <rowan@randb.com.au>
2010-05-08 15:30:22 +1000

    Edited bundle.php to make it compatible with PHP 5.3.

--- symphony/bundle.php
+++ symphony/bundle.php
@@ -1,36 +1,25 @@
 <?php
-
-	if(!defined('PHP_VERSION_ID')){
-    	$version = PHP_VERSION;
-    	define('PHP_VERSION_ID', ($version{0} * 10000 + $version{2} * 100 + $version{4}));
-	}
-
-	if (PHP_VERSION_ID >= 50300){
-	    //error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
-		// TODO: Revert this?
-		error_reporting(E_ALL);
-	} 
-	else{
-	    error_reporting(E_ALL & ~E_NOTICE);
-	    // TODO: Revert this?
-	    //error_reporting(E_ALL);
 	
-		// Bad Magic Quotes! You're not wanted here!
-		if(get_magic_quotes_gpc() === true) {
-			set_magic_quotes_runtime(false);
-			General::cleanArray($_SERVER);
-			General::cleanArray($_COOKIE);
-			General::cleanArray($_GET);
-			General::cleanArray($_POST);
-		}
-		
+	if(!defined('PHP_VERSION_ID')){
+		$version = PHP_VERSION;
+		define('PHP_VERSION_ID', ($version{0} * 10000 + $version{2} * 100 + $version{4}));
 	}
-
+	
+	error_reporting(E_ALL & ~E_NOTICE);
+	
+	// Bad Magic Quotes! You're not wanted here!
+	if (PHP_VERSION_ID < 50300 and get_magic_quotes_gpc() === true) {
+		set_magic_quotes_runtime(false);
+		General::cleanArray($_SERVER);
+		General::cleanArray($_COOKIE);
+		General::cleanArray($_GET);
+		General::cleanArray($_POST);
+	}
+	
 	require_once(DOCROOT . '/symphony/lib/include.utilities.php');	
 	require_once(DOCROOT . '/symphony/defines.php');
-
+	
 	if(!file_exists(CONFIG . '/db.xml')){
-		
 		if (file_exists(DOCROOT . '/install.php')){
 			header(sprintf('Location: %s/install.php', URL));
 			exit();
