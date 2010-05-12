dd4d4f33459ec259635ab8b212378beac4e20aa1
Rowan Lewis <rowan@randb.com.au>
2010-05-12 16:25:48 +1000

    General code cleanup, simplified and optimised filtering to use only one JOIN per DataSource.

--- extensions/field_date/fields/field.date.php
+++ extensions/field_date/fields/field.date.php
@@ -6,6 +6,8 @@
 		const REGEXP = 1;
 		const RANGE = 3;
 		const ERROR = 4;
+		
+		protected $join_handle;
 
 		function __construct(){
 			parent::__construct();
@@ -49,177 +51,9 @@
 			return true;
 		}
 
-		/*-------------------------------------------------------------------------
-			Utilities:
-		-------------------------------------------------------------------------*/
-
-		protected function __buildSimpleFilterSQL($filter, &$joins, &$where, $operation_type=DataSource::FILTER_OR) {
-			/* 	TODO: Fix Simple SQL
-				SPRINTF EATS UP %d
-				%%d% seems to make no difference
-
-			foreach($filter['value'] as $value) {
-				$filter['value'] = DataSource::prepareFilterValue($value);
-			}
-
-			if ($operation_type == DataSource::FILTER_AND) {
-				foreach ($value as $v) {
-					$where .= sprintf(
-						" AND DATE_FORMAT(t%1\$s.value, '%Y-%m-%%d %H:%i:%%s') %2\$s '%3\$s') ",
-						self::$key,
-						$filter['type'] == 'is-not' ? '<>' : '=',
-						DateTimeObj::get('Y-m-d h:i:s', strtotime($v))
-					);
-				}
-			}
-
-			else {
-				$parsed_values = array();
-				foreach($value as $v) $parsed_values[] = DateTimeObj::get('Y-m-d h:i:s', strtotime($v));
-
-				$where .= sprintf(
-					" AND DATE_FORMAT(t%1\$s.value, '%Y-%m-%%d %H:%i:%%s') %2\$s IN ('%3\$s') ",
-					self::$key,
-					$filter['type'] == 'is-not' ? 'NOT' : NULL,
-					implode("', '", $parsed_values)
-				);
-			}
-			*/
-		}
-
-		protected function __buildRangeFilterSQL($filter, &$joins, &$where, $operation_type=DataSource::FILTER_OR) {
-			/* 	TODO: Fix Range SQL
-				SPRINTF EATS UP %d
-				%%d% seems to make no difference
-
-
-			foreach($filter['value'] as $key => $value) {
-				$filter['value'][$key] = DataSource::prepareFilterValue($value);
-			}
-
-			if ($operation_type == DataSource::FILTER_AND) {
-				foreach ($filter['value']['start'] as $k => $v) {
-
-					$where .= sprintf(
-						" AND (
-							DATE_FORMAT(`t%1\$d.value, '%Y-%m-%d %H:%i:%s') >= '%2\$s'
-						  	AND DATE_FORMAT(`t%1\$d.value, '%Y-%m-%d %H:%i:%s') <= '%3\$s'
-						) ",
-						self::$key,
-						DateTimeObj::get('Y-m-d h:i:s', strtotime($v)),
-						DateTimeObj::get('Y-m-d h:i:s', strtotime($filter['value']['end'][$k]))
-					);
-				}
-
-
-			} else {
-
-				$tmp = array();
-
-				foreach ($filter['value']['start'] as $k => $v) {
-					$tmp[] .= sprintf(
-						" AND (
-							DATE_FORMAT(`t%1$s.value, '%Y-%m-%d %H:%i:%s') >= '%s'
-						  	AND DATE_FORMAT(`t%1$s.value, '%Y-%m-%d %H:%i:%s') <= '%s'
-						) ",
-						self::$key,
-						DateTimeObj::get('Y-m-d h:i:s', strtotime($v)),
-						DateTimeObj::get('Y-m-d h:i:s', strtotime($filter['value']['end'][$k]))
-					);
-				}
-
-				$where .= " AND (" . implode(' OR ', $tmp). ") ";
-
-			}
-			*/
-
-		}
-
-		protected static function __cleanFilterString($string){
-			$string = trim($string);
-			$string = trim($string, '-/');
-
-			return $string;
-		}
-
-		protected static function __parseFilter(&$string){
-/*
-			$string = self::__cleanFilterString($string);
-
-			## Check its not a regexp
-			if(preg_match('/^regexp:/i', $string)){
-				$string = str_replace('regexp:', '', $string);
-				return self::REGEXP;
-			}
-
-			## Look to see if its a shorthand date (year only), and convert to full date
-			elseif(preg_match('/^(1|2)\d{3}$/i', $string)){
-				$string = "$string-01-01 to $string-12-31";
-			}
-
-			## Human friendly terms
-			elseif(preg_match('/^(equal to or )?(earlier|later) than (.*)$/i', $string, $match)){
-
-				$string = $match[3];
-
-				if(!self::__isValidDateString($string)) return self::ERROR;
-
-				$time = strtotime($string);
-				if($match[1] == "equal to or "){
-					$later = DateTimeObj::get('Y-m-d H:i:s', $time);
-					$earlier = $later;
-				}
-				else {
-					$later = DateTimeObj::get('Y-m-d H:i:s', $time+1);
-					$earlier = DateTimeObj::get('Y-m-d H:i:s', $time-1);
-				}
-				switch($match[2]){
-					case 'later': $string = $later . ' to 2038-01-01'; break;
-					case 'earlier': $string = '1970-01-03 to ' . $earlier; break;
-				}
-
-			}
-
-			## Look to see if its a shorthand date (year and month), and convert to full date
-			elseif(preg_match('/^(1|2)\d{3}[-\/]\d{1,2}$/i', $string)){
-
-				$start = "{$string}-01";
-
-				if(!self::__isValidDateString($start)) return self::ERROR;
-
-				$string = "{$start} to {$string}-" . date('t', strtotime($start));
-			}
-
-			## Match for a simple date (Y-m-d), check its ok using checkdate() and go no further
-			elseif(!preg_match('/to/i', $string)){
-
-				if(preg_match('/^(1|2)\d{3}[-\/]\d{1,2}[-\/]\d{1,2}$/i', $string)){
-					$string = "{$string} to {$string}";
-				}
-
-				else{
-					if(!self::__isValidDateString($string)) return self::ERROR;
-
-					$string = DateTimeObj::get('Y-m-d H:i:s', strtotime($string));
-					return self::SIMPLE;
-				}
-			}
-
-			## Parse the full date range and return an array
-
-			if(!$parts = preg_split('/to/', $string, 2, PREG_SPLIT_NO_EMPTY)) return self::ERROR;
-
-			$parts = array_map(array('self', '__cleanFilterString'), $parts);
-
-			list($start, $end) = $parts;
-
-			if(!self::__isValidDateString($start) || !self::__isValidDateString($end)) return self::ERROR;
-
-			$string = array('start' => $start, 'end' => $end);
-
-			return self::RANGE;
-*/
-		}
+	/*-------------------------------------------------------------------------
+		Utilities:
+	-------------------------------------------------------------------------*/
 
 		protected static function __isValidDateString($string){
 			$string = trim($string);
@@ -233,9 +67,9 @@
 			return true;
 		}
 
-		/*-------------------------------------------------------------------------
-			Settings:
-		-------------------------------------------------------------------------*/
+	/*-------------------------------------------------------------------------
+		Settings:
+	-------------------------------------------------------------------------*/
 
 		public function findDefaultSettings(array &$fields){
 			if(!isset($fields['pre-populate'])) $fields['pre-populate'] = 'yes';
@@ -265,9 +99,9 @@
 
 		}
 
-		/*-------------------------------------------------------------------------
-			Publish:
-		-------------------------------------------------------------------------*/
+	/*-------------------------------------------------------------------------
+		Publish:
+	-------------------------------------------------------------------------*/
 
 		public function displayPublishPanel(SymphonyDOMElement $wrapper, MessageStack $errors, Entry $entry = null, $data = null){
 			$name = $this->{'element-name'};
@@ -295,9 +129,9 @@
 			$wrapper->appendChild($label);
 		}
 
-		/*-------------------------------------------------------------------------
-			Input:
-		-------------------------------------------------------------------------*/
+	/*-------------------------------------------------------------------------
+		Input:
+	-------------------------------------------------------------------------*/
 
 		public function processData($data, Entry $entry=NULL){
 
@@ -386,16 +220,8 @@
 			);
 		}
 
-		//	TODO: Revisit this.
-		//	Reason being that now that you don't actually enter 'date to date' and instead that would come
-		//	across as two different filters, [later than] date & [earlier than] date. Therefore this code doesn't
-		//	have to be as 'split here and do magic' as it is now.
-		//	The problem then is that what is a simple BETWEEN statement now is a bunch of joins and >=/<= operads
-		//	which is slow
 		public function buildFilterQuery($filter, &$joins, array &$where, Register $parameter_output = null) {
 			$filter = $this->processFilter($filter);
-			$filter_join = DataSource::FILTER_OR;
-			$values = DataSource::prepareFilterValue($filter->value, $parameter_output, $filter_join);
 			$db = Symphony::Database();
 			$statements = array();
 			
@@ -411,22 +237,21 @@
 				case 'later-than-or-equal':		$operator = '<='; break;
 			}
 			
-			if ($filter_join == DataSource::FILTER_OR) {
-				$handle = $this->buildFilterJoin($joins);
+			if (empty($this->last_handle)) {
+				$this->join_handle = $this->buildFilterJoin($joins);
 			}
 			
-			foreach ($values as $index => $value) {
-				if ($filter_join != DataSource::FILTER_OR) {
-					$handle = $this->buildFilterJoin($joins);
-				}
-				
-				$value = DateTimeObj::toGMT($value);
-				
-				$statements[] = $db->prepareQuery(
-					"%d {$operator} UNIX_TIMESTAMP({$handle}.value)",
-					array($value)
-				);
-			}
+			$handle = $this->join_handle;
+			
+			$value = DataSource::replaceParametersInString(
+				trim($filter->value), $parameter_output
+			);
+			$value = DateTimeObj::toGMT($value);
+			
+			$statements[] = $db->prepareQuery(
+				"%d {$operator} UNIX_TIMESTAMP({$handle}.value)",
+				array($value)
+			);
 
 			if (empty($statements)) return true;
 
@@ -443,10 +268,9 @@
 			return true;
 		}
 
-
-		/*-------------------------------------------------------------------------
-			Grouping:
-		-------------------------------------------------------------------------*/
+	/*-------------------------------------------------------------------------
+		Grouping:
+	-------------------------------------------------------------------------*/
 
 		public function groupRecords($records){
 
