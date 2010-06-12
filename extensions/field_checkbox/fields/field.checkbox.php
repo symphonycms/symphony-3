3ded7c464df947e137b5ff6c13ec23d09728c109
Alistair Kearney <alistair@21degrees.com.au>
2010-06-13 09:56:22 +1000

    Checkbox trying to use 'label' for publish area. Changed to 'name'

--- extensions/field_checkbox/fields/field.checkbox.php
+++ extensions/field_checkbox/fields/field.checkbox.php
@@ -116,7 +116,7 @@
 			$input = Widget::Input('fields['.$this->{'element-name'}.']', 'yes', 'checkbox', ($value == 'yes' ? array('checked' => 'checked') : array()));
 
 			$label->appendChild($input);
-			$label->appendChild(new DOMText(($this->{'publish-label'} != NULL ? $this->{'publish-label'} : $this->{'label'})));
+			$label->appendChild(new DOMText((isset($this->{'publish-label'}) && strlen(trim($this->{'publish-label'})) > 0 ? $this->{'publish-label'} : $this->{'name'})));
 
 			if ($errors->valid()) {
 				$label = Widget::wrapFormElementWithError($label, $errors->current()->message);
