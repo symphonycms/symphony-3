c02b74e4bb173a2e63b0802ff6ab178318cc0709
Rowan Lewis <rowan@randb.com.au>
2010-06-30 13:47:26 +1000

    Fixed selectable table bug.

--- symphony/assets/js/symphony.selectable.js
+++ symphony/assets/js/symphony.selectable.js
@@ -56,7 +56,13 @@
 			
 			// Start:
 			items.live('mousedown', function(event) {
-				if (event.button != (jQuery.browser.msie != undefined) || event.target.tagName.toLowerCase() == 'a') {
+				if (
+					event.button != (jQuery.browser.msie != undefined)
+					|| (
+						event.target.tagName.toLowerCase() == 'a'
+						|| event.target.tagName.toLowerCase() == 'input'
+					)
+				) {
 					items.removeClass('changing');
 					state = null;
 					return;
